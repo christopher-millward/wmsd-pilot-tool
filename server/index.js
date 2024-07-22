1040ad4973aed7967b665414cf0a977c00f28424
cmillward13 <chris.millward13@hotmail.com>
2024-07-22 13:54:30 -0400

    Reconfig CORS origin for production.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // Configure CORS
 const corsOptions = {
-    origin: '*', // Change this to specific origins in production.
+    origin: process.env.FRONTEND_ORIGIN, // Change to '*' in development.
     methods: 'POST,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
