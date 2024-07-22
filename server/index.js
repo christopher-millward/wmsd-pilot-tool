cb352da1881198905b0c2a8b13406d1acf8da261
cmillward13 <chris.millward13@hotmail.com>
2024-07-22 13:29:09 -0400

    Reconfigured for CORS preflight.

--- server/index.js
+++ server/index.js
@@ -7,16 +7,17 @@ const port = process.env.PORT || 5000;
 
 // Configure CORS
 const corsOptions = {
-    origin: process.env.FRONTEND_ORIGIN,
-    methods: 'POST',
+    origin: '*', // Change this to specific origins in production.
+    methods: 'POST,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
-    optionsSuccessStatus: 204,
-};
+    optionsSuccessStatus: 204
+  };
 
 // Middleware
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // handle preflight requests
+app.options('*', cors(corsOptions));
 
 // Import Routes
 const questionnaireRoutes = require('./routes/questionnaire');
