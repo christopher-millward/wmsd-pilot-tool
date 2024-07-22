fa9b194d7345efcec216c6e9d4492155b4b458f7
cmillward13 <chris.millward13@hotmail.com>
2024-07-22 14:01:50 -0400

    Reconfigured CORS origin.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // Configure CORS
 const corsOptions = {
-    origin: process.env.FRONTEND_ORIGIN, // Change to '*' in development.
+    origin: '*', // Change to '*' in development.
     methods: 'POST,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
